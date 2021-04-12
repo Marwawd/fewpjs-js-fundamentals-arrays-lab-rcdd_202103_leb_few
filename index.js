7d3575df28807148a5d479562f8a588fe6f92b89
Marwa  Awwad <marwaawwad10@gmail.com>
2021-04-12 13:28:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ pets=["Milo", "Otis", "Garfield"];
 function append(pets){
 pets.push("Odie");}
 append(pets);
+
+//function prepend(pets){
+//  pets.unshift(pets);
+//}
+
 //pets.splice(4, 1, 'May');
